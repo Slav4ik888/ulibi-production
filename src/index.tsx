6e0056c7bee490a01e525e8a1037011fb8c04449
Vyacheslav <korzan.va@mail.ru>
2023-04-09 20:55:03 +0600

    lesson 62 add netlify.toml

--- src/index.tsx
+++ src/index.tsx
@@ -43,4 +43,4 @@ render(
 // "configurations": {
 
 
-// git add . && git commit -m "lesson 62 start" && git push -u origin main
+// git add . && git commit -m "lesson 62 add netlify.toml" && git push -u origin main
